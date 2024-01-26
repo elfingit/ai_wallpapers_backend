fd759cac026f9287d26f733e95bdf2fb4432317d
Andrey Sergeev <andreylong@gmail.com>
2024-01-26 09:48:29 +0100

    create gallery - docs

--- app/Http/Controllers/Api/V1/GalleryController.php
+++ app/Http/Controllers/Api/V1/GalleryController.php
@@ -6,12 +6,72 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Gallery\AddRequest;
 use App\Library\Gallery\Commands\CreateGalleryCommand;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class GalleryController extends Controller
 {
-    public function store(AddRequest $request)
+    /**
+     * @api {post} /api/v1/gallery Add picture
+     * @apiName Add picture
+     * @apiGroup Gallery
+     * @apiDescription Endpoint for add picture
+     * @apiVersion 1.0.0
+     *
+     * @apiHeader {String} Content-Type application/json
+     * @apiHeader {String} Accept application/json
+     * @apiHeader {String} X-App-Locale en
+     * @apiHeader {String} Authorization Bearer
+     *
+     * @apiBody {File} file picture file
+     * @apiBody {String} prompt picture prompt
+     * @apiBody {String[]} tag array of tags (max 10)
+     *
+     * @apiHeaderExample {String} Header-Example:
+     *  {
+     *       Accept: application/json
+     *       Content-Type: application/json
+     *       X-App-Locale: pl
+     *       Authorization: Bearer 2|zXp0DMrIjSpyfdbJwO5CCjyn9loYjjcZ5GjZdjHVec126865
+     *  }
+     *
+     * @apiErrorExample {json} Validation-Error:
+     *   HTTP/1.1 422
+     *   {
+     *       "message": "The file field is required. (and 2 more errors)",
+     *       "errors": {
+     *           "errors": {
+     *               "file": [
+     *                   "The file field is required."
+     *               ],
+     *               "prompt": [
+     *                   "The prompt field is required."
+     *               ],
+     *              "tag": [
+     *                  "The tag field is required."
+     *             ]
+     *       }
+     *   }
+     *
+     * @apiErrorExample {json} Forbidden:
+     *  HTTP/1.1 403 Forbidden
+     *  {
+     *      "message": "Forbidden."
+     *  }
+     *
+     * @apiErrorExample {json} Auth-Error:
+     *   HTTP/1.1 401
+     *   {
+     *       message: "Unauthenticated"
+     *   }
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 201 OK
+     */
+    public function store(AddRequest $request): JsonResponse
     {
         $command = CreateGalleryCommand::createFromDto($request->getDto());
         \CommandBus::dispatch($command);
+
+        return response()->json(status: 201);
     }
 }
