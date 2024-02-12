9a5bcaa9ad4489c049c401e03d4d701107c01515
Andrey Sergeev <andreylong@gmail.com>
2024-02-12 11:27:47 +0100

    terms and conditions

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/terms_of_conditions.html', function () {
+    $locale = \App::getLocale();
+    return view($locale . '/terms_of_conditions');
 });
