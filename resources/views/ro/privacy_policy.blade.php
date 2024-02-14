0ec5ffe7e31be472b2ba52ba4235273b5b74d937
Andrey Sergeev <andreylong@gmail.com>
2024-02-14 11:16:40 +0100

    terms of conditions - romanian

---
+++ resources/views/ro/privacy_policy.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Privacy Policy</title>
+</head>
+<body style="background: #0A314B; color: white;">
+<h1>Privacy Policy</h1>
+<h2>1. Introduction</h2>
+<p>
+    Welcome to AI Wallpapers ("we", "us", "our").
+    We are committed to protecting the privacy and security of your personal information.
+    This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our mobile application ("App").
+</p>
+<h2>2. Information We Collect</h2>
+<p>We may collect personal identification information in various ways, including, but not limited to, when you:</p>
+<ul>
+    <li>Use our App</li>
+    <li>Register an account</li>
+    <li>Fill out a form</li>
+    <li>Use in-app purchases</li>
+    <li>Interact with other app functionalities</li>
+</ul>
+<p>
+    The types of information collected may include your name, email address, phone number, and any other information you choose to provide.
+</p>
+<h2>3. How We Use Your Information</h2>
+<p>The information we collect may be used in the following ways:</p>
+<ul>
+    <li>To provide, operate, and maintain our App</li>
+    <li>To improve, personalize, and expand our App</li>
+    <li>To understand and analyze how you use our App</li>
+    <li>To develop new products, services, features, and functionality</li>
+    <li>To provide, operate, and maintain our App</li>
+    <li>To communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the App, and for marketing and promotional purposes</li>
+    <li>To send you emails</li>
+</ul>
+<h2>4. Sharing Your Information</h2>
+<p>We may share your information with third parties in the following situations:</p>
+<ul>
+    <li>With your consent</li>
+    <li>For legal reasons, if we believe disclosure is in accordance with or required by any applicable law, regulation, or legal process</li>
+    <li>With our trusted services providers who work on our behalf, do not have an independent use of the information we disclose to them, and have agreed to adhere to the rules set forth in this Privacy Policy</li>
+</ul>
+<h2>5. Security</h2>
+<p>
+    We value your trust in providing us your personal information, thus we strive to use commercially acceptable means of protecting it. However, remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.
+</p>
+<h2>6. Links to Other Sites</h2>
+<p>
+    Our App may contain links to other sites.
+    If you click on a third-party link, you will be directed to that site.
+    Note that these external sites are not operated by us.
+    Therefore, we strongly advise you to review the Privacy Policy of these websites.
+    We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
+</p>
+<h2>7. Children's Privacy</h2>
+<p>
+    Our App does not address anyone under the age of 13.
+    We do not knowingly collect personally identifiable information from children under 13.
+    In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers.
+    If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.
+</p>
+<h2>8. Changes to This Privacy Policy</h2>
+<p>
+    We may update our Privacy Policy from time to time.
+    Thus, you are advised to review this page periodically for any changes.
+    We will notify you of any changes by posting the new Privacy Policy on this page.
+    These changes are effective immediately after they are posted on this page.
+</p>
+<h2>9. Contact Us</h2>
+<p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us via email <a href="mailto:admin@nuntech.com">admin@nuntech.com</a>.</p>
+</body>
+</html>
---
+++ resources/views/ro/terms_of_conditions.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Termeni și condiții</title>
+</head>
+<body style="background: #0A314B; color: white;">
+<h1>Termeni și condiții</h1>
+<h2>1. Introducere</h2>
+<p>
+    Bine ați venit la AI Wallpapers ("noi", "ne", "al nostru").
+    Accesând sau utilizând aplicația noastră mobilă ("Aplicația"), dumneavoastră ("utilizatorul", "dumneavoastră") sunteți de acord să fiți legat de acești Termeni și Condiții ("Termeni").
+    Dacă nu sunteți de acord cu acești Termeni, nu aveți permisiunea de a utiliza Aplicația noastră.
+</p>
+<h2>2. Politica de Confidențialitate</h2>
+<p>
+    Politica noastră de Confidențialitate, care detaliază modul în care colectăm, utilizăm și protejăm datele dumneavoastră personale, este încorporată în acești Termeni prin
+    <a href="/privacy_policy.html">referință.</a>
+    Vă rugăm să citiți cu atenție Politica noastră de Confidențialitate înainte de a utiliza Aplicația.
+</p>
+<h2>3. Licența de Utilizare a Aplicației</h2>
+<p>
+    Vă acordăm o licență revocabilă, ne-exclusivă, ne-transferabilă, limitată pentru a descărca, instala și utiliza Aplicația pentru uzul dumneavoastră personal, non-comercial, supus acestor Termeni.
+</p>
+<h2>4. Conturi de Utilizatori</h2>
+<p>
+    Dacă orice parte a Aplicației noastre vă cere să creați un cont, trebuie să furnizați informații exacte și complete.
+    Sunteți responsabil pentru menținerea confidențialității informațiilor contului dumneavoastră, inclusiv parola, și pentru toate activitățile care au loc sub contul dumneavoastră.
+</p>
+<h2>5. Conduita Utilizatorului</h2>
+<p>
+    Sunteți de acord să utilizați Aplicația noastră numai în scopuri legale și într-un mod care nu încalcă drepturile, nu restricționează sau inhibă utilizarea și bucuria Aplicației de către oricare alt utilizator.
+    Comportamentul interzis include hărțuirea sau cauzarea de disconfort sau inconveniențe oricărui alt utilizator, transmiterea de conținut obscen sau ofensator, sau perturbarea fluxului normal de dialog în cadrul Aplicației noastre.
+</p>
+<h2>6. Drepturi de Proprietate Intelectuală</h2>
+<p>
+    Aplicația și conținutul său original, caracteristicile și funcționalitățile sunt și vor rămâne proprietatea exclusivă a Nuntechs și a licențiatorilor săi.
+    Proprietatea noastră intelectuală nu trebuie utilizată într-un mod neautorizat de noi.
+</p>
+<h2>7. Încheiere</h2>
+<p>
+    Putem termina sau suspenda contul dumneavoastră și interzice accesul la Aplicație imediat, fără notificare prealabilă sau răspundere, la discreția noastră exclusivă, pentru orice motiv și fără limitare, inclusiv, dar fără a se limita la o încălcare a Termenilor.
+</p>
+<h2>8. Limitarea Răspunderii</h2>
+<p>
+    In no event will Nuntechs, nor its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential, or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from your use of the App or services.
+</p>
+<h2>9. Modificări ale Termenilor</h2>
+<p>
+    Ne rezervăm dreptul, la discreția noastră exclusivă, de a modifica sau înlocui acești Termeni în orice moment.
+    Continuând să accesați sau să utilizați Aplicația noastră după ce aceste revizuiri devin efective, sunteți de acord să fiți legat de termenii revizuiți.
+</p>
+<h2>10. Legea Aplicabilă</h2>
+<p>
+    Acești Termeni vor fi guvernați și interpretați în conformitate cu legile Poloniei, fără a ține cont de prevederile sale privind conflictele de legi.
+</p>
+<h2>11. Contact Us</h2>
+    <p>
+        Dacă aveți întrebări despre acești Termeni, vă rugăm să ne contactați prin e-mail la  <a href="mailto:admin@nuntech.com">admin@nuntech.com</a>.
+    </p>
+
+
+</body>
+</html>
