88c6d0589908925d7b6eafe1228e4e2326e3df20
Andrey Sergeev <andreylong@gmail.com>
2024-02-14 09:11:51 +0100

    french privacy policy

---
+++ resources/views/fr/privacy_policy.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Politique de Confidentialité</title>
+</head>
+<body style="background: #0A314B; color: white;">
+<h1>Politique de Confidentialité</h1>
+<h2>1. Introduction</h2>
+<p>
+    Bienvenue chez AI Wallpapers (« nous », « notre », « nos »).
+    Nous nous engageons à protéger la vie privée et la sécurité de vos informations personnelles.
+    Cette Politique de Confidentialité explique comment nous collectons, utilisons, divulguons et protégeons vos informations lorsque vous utilisez notre application mobile (« Appli »).
+</p>
+<h2>2. Informations que Nous Collectons</h2>
+<p>
+    Nous pouvons collecter des informations d'identification personnelle de diverses manières, y compris, mais sans s'y limiter, lorsque vous:
+</p>
+<ul>
+    <li>Utilisez notre Appli</li>
+    <li>Enregistrez un compte</li>
+    <li>Remplissez un formulaire</li>
+    <li>Utilisez des achats intégrés</li>
+    <li>Interagissez avec d'autres fonctionnalités de l'appli</li>
+</ul>
+<p>
+    Les types d'informations collectées peuvent inclure votre nom, adresse e-mail, numéro de téléphone et toute autre information que vous choisissez de fournir.
+</p>
+<h2>3. Comment Nous Utilisons Vos Informations</h2>
+<p>
+    Les informations que nous collectons peuvent être utilisées de la manière suivante:
+</p>
+<ul>
+    <li>Pour fournir, exploiter et maintenir notre Appli</li>
+    <li>Pour améliorer, personnaliser et étendre notre Appli</li>
+    <li>Pour comprendre et analyser comment vous utilisez notre Appli</li>
+    <li>Pour développer de nouveaux produits, services, fonctionnalités et fonctionnalités</li>
+    <li>Pour communiquer avec vous, directement ou par l'intermédiaire de l'un de nos partenaires, y compris pour le service client, pour vous fournir des mises à jour et d'autres informations relatives à l'Appli, et à des fins de marketing et promotionnelles</li>
+    <li>Pour vous envoyer des e-mails</li>
+</ul>
+<h2>4. Partage de Vos Informations</h2>
+<p>Nous pouvons partager vos informations avec des tiers dans les situations suivantes:</p>
+<ul>
+    <li>Avec votre consentement</li>
+    <li>Pour des raisons légales, si nous estimons que la divulgation est conforme à ou requise par toute loi, réglementation ou procédure légale applicable</li>
+    <li>Avec nos prestataires de services de confiance qui travaillent en notre nom, n'ont pas d'utilisation indépendante des informations que nous leur divulguons, et ont accepté de se conformer aux règles énoncées dans cette Politique de Confidentialité</li>
+</ul>
+<h2>5. Sécurité</h2>
+<p>
+    Nous valorisons votre confiance en nous fournissant vos informations personnelles, ainsi nous nous efforçons d'utiliser des moyens commercialement acceptables pour la protéger. Cependant, rappelez-vous qu'aucune méthode de transmission sur Internet, ou méthode de stockage électronique n'est sûre à 100 % et fiable, et nous ne pouvons pas garantir sa sécurité absolue.
+</p>
+<h2>6. Liens vers d'Autres Sites</h2>
+<p>
+    Notre Appli peut contenir des liens vers d'autres sites.
+    Si vous cliquez sur un lien de tiers, vous serez dirigé vers ce site.
+    Notez que ces sites externes ne sont pas exploités par nous.
+    Par conséquent, nous vous conseillons vivement de revoir la Politique de Confidentialité de ces sites Web.
+    Nous n'avons aucun contrôle sur et n'assumons aucune responsabilité quant au contenu, politiques de confidentialité ou pratiques de tout site ou service tiers.
+</p>
+<h2>7. Confidentialité des Enfants</h2>
+<p>
+    Notre Appli ne s'adresse pas à quiconque âgé de moins de 13 ans.
+    Nous ne collectons pas sciemment d'informations personnelles identifiables auprès d'enfants de moins de 13 ans.
+    Dans le cas où nous découvrons qu'un enfant de moins de 13 ans nous a fourni des informations personnelles, nous les supprimons immédiatement de nos serveurs.
+    Si vous êtes un parent ou un tuteur et que vous êtes conscient que votre enfant nous a fourni des informations personnelles, veuillez nous contacter afin que nous puissions prendre les mesures nécessaires.
+</p>
+<h2>8. Modifications de Cette Politique de Confidentialité</h2>
+<p>
+    Nous pouvons mettre à jour notre Politique de Confidentialité de temps à autre.
+    Ainsi, vous êtes invités à consulter cette page périodiquement pour tout changement.
+    Nous vous informerons de tout changement en publiant la nouvelle Politique de Confidentialité sur cette page.
+    Ces changements sont effectifs immédiatement après leur publication sur cette page.
+</p>
+<h2>9. Contactez-Nous</h2>
+<p>
+    Si vous avez des questions ou des suggestions concernant notre Politique de Confidentialité, n'hésitez pas à nous contacter par e-mail à <a href="mailto:admin@nuntech.com">admin@nuntech.com</a>.
+</p>
+</body>
+</html>
