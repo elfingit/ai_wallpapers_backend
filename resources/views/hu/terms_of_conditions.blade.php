28f189d0002ec813784c3c5978d308bbf69de0f2
Andrey Sergeev <andreylong@gmail.com>
2024-02-15 09:16:30 +0100

    privacy policy - hungarian

---
+++ resources/views/hu/terms_of_conditions.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Terms and Conditions</title>
+</head>
+<body style="background: #0A314B; color: white;">
+<h1>Terms and Conditions</h1>
+<h2>1. Introduction</h2>
+<p>
+    Welcome to AI Wallpapers ("we", "us", "our").
+    By accessing or using our mobile application ("App"), you ("user", "you") agree to be bound by these Terms and Conditions ("Terms").
+    If you do not agree with these Terms, you may not use our App.
+</p>
+<h2>2. Privacy Policy</h2>
+<p>
+    Our Privacy Policy, which details how we collect, use, and protect your personal data, is incorporated into these Terms by
+    <a href="/privacy_policy.html">reference.</a>
+    Please read our Privacy Policy carefully before using our App.
+</p>
+<h2>3. License to Use the App,</h2>
+<p>
+    We grant you a revocable, non-exclusive, non-transferable, limited license to download, install, and use the App for your personal, non-commercial use, subject to these Terms.
+</p>
+<h2>4. User Accounts</h2>
+<p>
+    If any portion of our App requires you to create an account, you must provide accurate and complete information.
+    You are responsible for maintaining the confidentiality of your account information, including your password, and for all activities that occur under your account.
+</p>
+<h2>5. User Conduct</h2>
+<p>
+    You agree to use our App only for lawful purposes and in a way that does not infringe the rights of, restrict or inhibit anyone else's use and enjoyment of the App.
+    Prohibited behavior includes harassing or causing distress or inconvenience to any other user, transmitting obscene or offensive content, or disrupting the normal flow of dialogue within our App.
+</p>
+<h2>6. Intellectual Property Rights,</h2>
+<p>
+    The App and its original content, features, and functionality are and will remain the exclusive property of Nuntechs and its licensors.
+    Our intellectual property must not be used in any manner not authorized by us.
+</p>
+<h2>7. Termination</h2>
+<p>
+    We may terminate or suspend your account and bar access to the App immediately, without prior notice or liability, under our sole discretion, for any reason whatsoever and without limitation, including but not limited to a breach of the Terms.
+</p>
+<h2>8. Limitation of Liability</h2>
+<p>
+    In no event will Nuntechs, nor its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential, or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from your use of the App or services.
+</p>
+<h2>9. Changes to Terms</h2>
+<p>
+    We reserve the right, at our sole discretion, to modify or replace these Terms at any time.
+    By continuing to access or use our App after those revisions become effective, you agree to be bound by the revised terms.
+</p>
+<h2>10. Governing Law</h2>
+<p>
+    These Terms shall be governed and construed in accordance with the laws of Poland, without regard to its conflict of law provisions.
+</p>
+<h2>11. Contact Us</h2>
+    <p>
+        If you have any questions about these Terms, please contact us via email <a href="mailto:admin@nuntech.com">admin@nuntech.com</a>.
+    </p>
+
+
+</body>
+</html>
