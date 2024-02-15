c168edb67fcacdef10f088714d8a3a4c66e49039
Andrey Sergeev <andreylong@gmail.com>
2024-02-15 09:32:03 +0100

    terms of use - hungarian

--- resources/views/hu/terms_of_conditions.blade.php
+++ resources/views/hu/terms_of_conditions.blade.php
@@ -2,61 +2,61 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Terms and Conditions</title>
+    <title>Felhasználási Feltételek</title>
 </head>
 <body style="background: #0A314B; color: white;">
-<h1>Terms and Conditions</h1>
-<h2>1. Introduction</h2>
+<h1>Felhasználási Feltételek</h1>
+<h2>1. Bevezetés</h2>
 <p>
-    Welcome to AI Wallpapers ("we", "us", "our").
-    By accessing or using our mobile application ("App"), you ("user", "you") agree to be bound by these Terms and Conditions ("Terms").
-    If you do not agree with these Terms, you may not use our App.
+    Üdvözöljük az AI Wallpapersnél („mi”, „minket”, „miénk”).
+    Mobilalkalmazásunk („Alkalmazás”) elérésével vagy használatával Ön („felhasználó”, „Ön”) elfogadja, hogy ezekhez a Felhasználási Feltételekhez („Feltételek”) kötődik.
+    Ha nem ért egyet ezekkel a Feltételekkel, akkor nem használhatja Alkalmazásunkat.
 </p>
-<h2>2. Privacy Policy</h2>
+<h2>2. Adatvédelmi Szabályzat</h2>
 <p>
-    Our Privacy Policy, which details how we collect, use, and protect your personal data, is incorporated into these Terms by
-    <a href="/privacy_policy.html">reference.</a>
-    Please read our Privacy Policy carefully before using our App.
+    Adatvédelmi Szabályzatunk, amely részletezi, hogyan gyűjtjük, használjuk és védelmezzük személyes adatait, be van építve ezekbe a Feltételekbe
+    <a href="/privacy_policy.html">hivatkozással.</a>
+    Kérjük, Alkalmazásunk használata előtt figyelmesen olvassa el Adatvédelmi Szabályzatunkat.
 </p>
-<h2>3. License to Use the App,</h2>
+<h2>3. Licenc az Alkalmazás használatára</h2>
 <p>
-    We grant you a revocable, non-exclusive, non-transferable, limited license to download, install, and use the App for your personal, non-commercial use, subject to these Terms.
+    Megadunk Önnek egy visszavonható, nem kizárólagos, átruházhatatlan, korlátozott licencet az Alkalmazás letöltésére, telepítésére és személyes, nem kereskedelmi célú használatára, ezen Feltételeknek megfelelően.
 </p>
-<h2>4. User Accounts</h2>
+<h2>4. Felhasználói Fiókok</h2>
 <p>
-    If any portion of our App requires you to create an account, you must provide accurate and complete information.
-    You are responsible for maintaining the confidentiality of your account information, including your password, and for all activities that occur under your account.
+    Ha Alkalmazásunk bármely része fiók létrehozását igényli, pontos és teljes információt kell megadnia.
+    Felelős az Ön fiókinformációinak, beleértve jelszavát is, titokban tartásáért, valamint fiókja minden tevékenységéért.
 </p>
-<h2>5. User Conduct</h2>
+<h2>5. Felhasználói Magatartás</h2>
 <p>
-    You agree to use our App only for lawful purposes and in a way that does not infringe the rights of, restrict or inhibit anyone else's use and enjoyment of the App.
-    Prohibited behavior includes harassing or causing distress or inconvenience to any other user, transmitting obscene or offensive content, or disrupting the normal flow of dialogue within our App.
+    Ön vállalja, hogy Alkalmazásunkat csak törvényes célokra használja, és oly módon, amely nem sérti mások jogait, nem korlátozza vagy akadályozza mások Alkalmazásunk élvezetét.
+    A tiltott viselkedés magában foglalja más felhasználó zaklatását vagy kellemetlenség okozását, obszcén vagy sértő tartalom közvetítését, vagy az Alkalmazásunkon belüli normál párbeszéd zavarását.
 </p>
-<h2>6. Intellectual Property Rights,</h2>
+<h2>6. Szellemi Tulajdonjogok</h2>
 <p>
-    The App and its original content, features, and functionality are and will remain the exclusive property of Nuntechs and its licensors.
-    Our intellectual property must not be used in any manner not authorized by us.
+    Az Alkalmazás és eredeti tartalma, funkciói és funkcionalitása a Nuntechs és licencadóinak kizárólagos tulajdona marad.
+    Szellemi tulajdonunkat nem használhatja fel engedély nélkül.
 </p>
-<h2>7. Termination</h2>
+<h2>7. Megszüntetés</h2>
 <p>
-    We may terminate or suspend your account and bar access to the App immediately, without prior notice or liability, under our sole discretion, for any reason whatsoever and without limitation, including but not limited to a breach of the Terms.
+    Azonnali hatállyal, előzetes értesítés vagy felelősség nélkül, saját belátásunk szerint bármilyen okból kifolyólag, beleértve, de nem korlátozva a Feltételek megsértésére, megszüntethetjük vagy felfüggeszthetjük fiókját és az Alkalmazáshoz való hozzáférést.
 </p>
-<h2>8. Limitation of Liability</h2>
+<h2>8. Felelősségi Korlátozás</h2>
 <p>
-    In no event will Nuntechs, nor its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential, or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from your use of the App or services.
+    Semmilyen esetben sem lesz a Nuntechs, annak igazgatói, alkalmazottai, partnerei, ügynökei, beszállítói vagy társult vállalkozásai felelősek semmilyen közvetett, véletlen, különleges, következményes vagy büntető károkért, beleértve korlátozás nélkül a nyereségvesztést, adatokat, használatot, jóakaratot vagy egyéb immateriális veszteségeket, amelyek az Alkalmazás vagy szolgáltatások használatából erednek.
 </p>
-<h2>9. Changes to Terms</h2>
+<h2>9. A Feltételek Változásai</h2>
 <p>
-    We reserve the right, at our sole discretion, to modify or replace these Terms at any time.
-    By continuing to access or use our App after those revisions become effective, you agree to be bound by the revised terms.
+    Fenntartjuk a jogot, hogy saját belátásunk szerint bármikor módosítsuk vagy helyettesítsük ezeket a Feltételeket.
+    Az ilyen módosítások hatálybalépése után az Alkalmazásunkhoz való hozzáférés vagy használat folytatásával Ön elfogadja a módosított feltételeket.
 </p>
-<h2>10. Governing Law</h2>
+<h2>10. Irányadó Jogi Rendszer</h2>
 <p>
-    These Terms shall be governed and construed in accordance with the laws of Poland, without regard to its conflict of law provisions.
+    Ezen Feltételeket Lengyelország törvényei szerint kell értelmezni és alkalmazni, figyelmen kívül hagyva annak jogi rendelkezéseit.
 </p>
-<h2>11. Contact Us</h2>
+<h2>11. Lépjen Velünk Kapcsolatba</h2>
     <p>
-        If you have any questions about these Terms, please contact us via email <a href="mailto:admin@nuntech.com">admin@nuntech.com</a>.
+        Ha kérdései vannak ezekkel a Feltételekkel kapcsolatban, kérjük, lépjen kapcsolatba velünk az <a href="mailto:admin@nuntech.com">admin@nuntech.com</a> e-mail címen.
     </p>
 
 
