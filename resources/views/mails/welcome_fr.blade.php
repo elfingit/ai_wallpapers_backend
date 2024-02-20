4db8dd3759bf3b95696c24449e16105756685dad
Andrey Sergeev <andreylong@gmail.com>
2024-02-20 12:32:42 +0100

    welcome mail - translations

---
+++ resources/views/mails/welcome_fr.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="font-family:roboto, 'helvetica neue', helvetica, arial, sans-serif">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title>AI Wallpapers</title>
+</head>
+<body>
+<h1>Bienvenue sur AI Wallpapers!</h1>
+<p>
+    Plongez dans un univers où la créativité n'a pas de limites et chaque fond d'écran raconte une histoire.
+    Avec AI Wallpapers, le fond d'écran de votre téléphone n'est plus juste un arrière-plan; c'est une œuvre d'art adaptée à votre style et à votre humeur.
+</p>
+<h2>Découvrez l'Inimaginable</h2>
+<p>
+    Notre collection pilotée par l'IA vous apporte un éventail infini de fonds d'écran dynamiques et époustouflants.
+    Des abstractions hypnotisantes aux paysages à couper le souffle, trouvez la scène parfaite qui résonne avec votre goût unique.
+</p>
+<h2>Innovez Votre Espace</h2>
+<p>
+    Personnalisez votre expérience avec des outils intuitifs qui vous permettent de modifier et d'affiner vos fonds d'écran choisis.
+    Rendez chaque design véritablement vôtre.
+</p>
+<h2>Rafraîchissez, Revitalisez, Réinventez</h2>
+<p>
+    Avec AI Wallpapers, votre appareil devient une toile de possibilités.
+    Explorez de nouvelles œuvres d'art chaque jour et laissez votre écran s'animer avec innovation et art.
+</p>
+<br/>
+<p>
+    Merci d'avoir choisi AI Wallpapers, où votre imagination rencontre notre innovation.
+    Préparez-vous à transformer votre expérience mobile.
+</p>
+</body>
+</html>
---
+++ resources/views/mails/welcome_ro.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="font-family:roboto, 'helvetica neue', helvetica, arial, sans-serif">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title>AI Wallpapers</title>
+</head>
+<body>
+<h1>Bine ați venit la AI Wallpapers!</h1>
+<p>
+    Scufundați-vă într-o lume unde creativitatea nu cunoaște limite și fiecare tapet spune o poveste.
+    Cu AI Wallpapers, fundalul telefonului dumneavoastră nu mai este doar un fundal; este o capodoperă adaptată stilului și dispoziției dumneavoastră.
+</p>
+<h2>Descoperiți Inimaginabilul</h2>
+<p>
+    Colecția noastră condusă de IA vă aduce o gamă infinită de tapete uluitoare, dinamice.
+    De la abstracțiuni hipnotizante la peisaje care îți taie respirația, găsiți scena perfectă care rezonază cu gustul dumneavoastră unic.
+</p>
+<h2>Inovați Spațiul Dumneavoastră</h2>
+<p>
+    Personalizați-vă experiența cu instrumente intuitive care vă permit să ajustați și să finisați tapetele alese.
+    Faceți fiecare design cu adevărat al dumneavoastră.
+</p>
+<h2>Reîmprospătați, Revigorați, Reinventați</h2>
+<p>
+    Cu AI Wallpapers, dispozitivul dumneavoastră devine o pânză de posibilități.
+    Explorați noi opere de artă în fiecare zi și lăsați ecranul să prindă viață cu inovație și artă.
+</p>
+<br/>
+<p>
+    Vă mulțumim că ați ales AI Wallpapers, unde imaginația dumneavoastră întâlnește inovația noastră.
+    Pregătiți-vă să transformați experiența mobilă.
+</p>
+</body>
+</html>
