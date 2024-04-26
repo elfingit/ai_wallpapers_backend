eaabd7e3dc0cda000d6de4472e10776236aacd89
Andrey Sergeev <andreylong@gmail.com>
2024-04-26 18:41:56 +0200

    in progress

--- app/Http/Controllers/Api/V1/UserDeviceController.php
+++ app/Http/Controllers/Api/V1/UserDeviceController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserDevice\AddRequest;
+use App\Library\UserDevice\Commands\CreateUserDeviceCommand;
 use Illuminate\Http\Request;
 
 class UserDeviceController extends Controller
 {
     public function store(AddRequest $request)
     {
-        return response()->json([
-            'message' => 'Device registration',
-        ]);
+        $dto = $request->getDto();
+        $command = CreateUserDeviceCommand::createFromPrimitive(
+            $dto->device_id,
+            $request->ip() ?? 'localhost',
+            $request->userAgent() ?? 'unknown'
+        );
+        \CommandBus::dispatch($command);
+
+        return response()->json(status: 201);
     }
 }
