a5173291bae5c0e32655d17197ff5913e50553b9
Andrey Sergeev <andreylong@gmail.com>
2024-06-17 12:48:11 +0200

    fiz again

--- app/Library/Billing/Handlers/AppleSubscriptionHandler.php
+++ app/Library/Billing/Handlers/AppleSubscriptionHandler.php
@@ -102,7 +102,7 @@ class AppleSubscriptionHandler extends ApplePurchaseHandler
             'price' => $claims->get('price'),
             'currency' => $claims->get('currency'),
             'start_date' => $claims->get('purchaseDate'),
-            'end_date' => $endDate,
+            'end_date' => $renewClaims->get('renewalDate'),
             'status' => $now->gt($endDate) ? SubscriptionStatusEnum::ACTIVE : SubscriptionStatusEnum::EXPIRED,
             'account_type' => is_null($command->userId) ? AccountTypeEnum::DEVICE : AccountTypeEnum::USER,
             'account_id' => $command->userId?->value(),
