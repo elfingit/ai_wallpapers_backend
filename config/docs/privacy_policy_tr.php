bba66f41cc1df8e2cae35071663d012db7facb38
Andrey Sergeev <andreylong@gmail.com>
2024-07-16 10:38:39 +0200

    turkish lang

---
+++ config/docs/privacy_policy_tr.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrei Siarheyeu <andreylong@gmail.com>
+ * Date: 14.03.24
+ * Time: 09:57
+ */
+
+return [
+    'title' => 'Gizlilik Politikası',
+    'sections' => [
+        [
+            'title' => '1. Giriş',
+            'content'=> '<p>' . env('PRODUCT_NAME') . ' ("biz", "bizi", "bizim") uygulamasına hoş geldiniz.
+                         Kişisel bilgilerinizin gizliliğini ve güvenliğini korumaya kararlıyız.
+                         Bu Gizlilik Politikası, mobil uygulamamızı ("Uygulama") kullandığınızda bilgilerinizi nasıl topladığımızı, kullandığımızı, ifşa ettiğimizi ve koruduğumuzu açıklar.</p>'
+        ], [
+            'title' => '2. Topladığımız Bilgiler',
+            'content' => '<p>Kişisel kimlik bilgilerini çeşitli şekillerde toplayabiliriz, bunlarla sınırlı olmamak üzere:</p>
+                          <ul>
+                              <li>Uygulamamızı kullanırken</li>
+                              <li>Bir hesap kaydederken</li>
+                              <li>Bir form doldururken</li>
+                              <li>Uygulama içi satın alımlar yaparken</li>
+                              <li>Diğer uygulama işlevleriyle etkileşim kurarken</li>
+                          </ul>
+                          <p>Toplanan bilgi türleri adınız, e-posta adresiniz, telefon numaranız ve vermeyi seçtiğiniz diğer bilgileri içerebilir.</p>'
+        ], [
+            'title' => '3. Bilgilerinizi Nasıl Kullanıyoruz',
+            'content' => '<p>Topladığımız bilgiler şu şekillerde kullanılabilir:</p>
+                          <ul>
+                              <li>Uygulamamızı sağlamak, işletmek ve sürdürmek için</li>
+                              <li>Uygulamamızı geliştirmek, kişiselleştirmek ve genişletmek için</li>
+                              <li>Uygulamamızı nasıl kullandığınızı anlamak ve analiz etmek için</li>
+                              <li>Yeni ürünler, hizmetler, özellikler ve işlevsellik geliştirmek için</li>
+                              <li>Uygulamamızı sağlamak, işletmek ve sürdürmek için</li>
+                              <li>Müşteri hizmetleri, uygulama ile ilgili güncellemeler ve diğer bilgileri sağlamak ve pazarlama ve promosyon amaçları için sizinle doğrudan veya ortaklarımız aracılığıyla iletişim kurmak için</li>
+                              <li>Size e-posta göndermek için</li>
+                          </ul>'
+        ], [
+            'title' => '4. Bilgilerinizi Paylaşma',
+            'content' => '<p>Bilgilerinizi aşağıdaki durumlarda üçüncü taraflarla paylaşabiliriz:</p>
+                          <ul>
+                              <li>Onayınızla</li>
+                              <li>Yasal nedenlerle, eğer ifşanın yürürlükteki herhangi bir yasa, düzenleme veya yasal süreçle uyumlu olduğuna veya gerektirdiğine inanıyorsak</li>
+                              <li>Bizim adımıza çalışan, bağımsız olarak kullanma hakkı olmayan ve bu Gizlilik Politikası\'nda belirtilen kurallara uymayı kabul etmiş güvenilir hizmet sağlayıcılarımızla</li>
+                          </ul>'
+        ], [
+            'title' => '5. Güvenlik',
+            'content' => '<p>Kişisel bilgilerinizi bize sağladığınız için güveninizi önemsiyoruz, bu yüzden korumak için ticari olarak kabul edilebilir yollar kullanmaya çalışıyoruz. Ancak, internet üzerinden iletim yöntemi veya elektronik depolama yöntemi %100 güvenli ve güvenilir değildir ve mutlak güvenliği garanti edemeyiz.</p>'
+        ], [
+            'title' => '6. Diğer Sitelerle Bağlantılar',
+            'content' => '<p>Uygulamamız diğer sitelere bağlantılar içerebilir.
+                          Bir üçüncü taraf bağlantısına tıkladığınızda, o siteye yönlendirileceksiniz.
+                          Bu harici sitelerin bizim tarafımızdan işletilmediğini unutmayın.
+                          Bu nedenle, bu web sitelerinin Gizlilik Politikalarını gözden geçirmenizi önemle tavsiye ederiz.
+                          Üçüncü taraf sitelerin veya hizmetlerin içeriği, gizlilik politikaları veya uygulamaları üzerinde hiçbir kontrolümüz yoktur ve sorumluluk kabul etmeyiz.</p>'
+        ], [
+            'title' => '7. Çocukların Gizliliği',
+            'content' => '<p>Uygulamamız 13 yaşın altındaki hiç kimseye hitap etmemektedir.
+                                13 yaşın altındaki çocuklardan bilerek kişisel olarak tanımlanabilir bilgi toplamıyoruz.
+                                13 yaşın altındaki bir çocuğun bize kişisel bilgi verdiğini tespit edersek, bunu derhal sunucularımızdan sileriz.
+                                Eğer bir ebeveyn veya veli iseniz ve çocuğunuzun bize kişisel bilgi sağladığının farkındaysanız, gerekli işlemleri yapabilmemiz için lütfen bizimle iletişime geçin.</p>'
+        ], [
+            'title' => '8. Bu Gizlilik Politikasındaki Değişiklikler',
+            'content' => '<p>Gizlilik Politikamızı zaman zaman güncelleyebiliriz.
+                          Bu nedenle, herhangi bir değişiklik için bu sayfayı periyodik olarak gözden geçirmeniz tavsiye edilir.
+                          Yeni Gizlilik Politikasını bu sayfada yayınlayarak sizi değişikliklerden haberdar edeceğiz.
+                          Bu değişiklikler, bu sayfada yayınlandıklarında hemen yürürlüğe girer.</p>'
+        ], [
+            'title' => '9. Bizimle İletişime Geçin',
+            'content' => '<p>Gizlilik Politikamız hakkında herhangi bir sorunuz veya öneriniz varsa, lütfen e-posta yoluyla bizimle iletişime geçmekten çekinmeyin <a href="mailto:admin@nuntech.com">admin@nuntech.com</a>.</p>'
+        ]
+    ]
+];
---
+++ config/docs/terms_of_conditions_tr.php
@@ -0,0 +1,80 @@
+<?php
+return [
+    'title' => 'Şartlar ve Koşullar',
+    'sections' => [
+        [
+            'title' => '1. Giriş',
+            'content' => '<p>
+    ' . env('PRODUCT_NAME') . ' ("biz", "bizi", "bizim") uygulamasına hoş geldiniz.
+    Mobil uygulamamıza ("Uygulama") erişerek veya kullanarak, bu Şartlar ve Koşullar ("Şartlar") ile bağlı kalmayı kabul edersiniz.
+    Bu Şartları kabul etmiyorsanız, Uygulamamızı kullanmayabilirsiniz.
+</p>',
+        ],
+        [
+            'title' => '2. Gizlilik Politikası',
+            'content' => '<p>
+    Kişisel verilerinizi nasıl topladığımızı, kullandığımızı ve koruduğumuzu detaylandıran Gizlilik Politikamız, bu Şartlara referansla dahil edilmiştir.
+    <a href="/privacy_policy.html">referans.</a>
+    Uygulamamızı kullanmadan önce Gizlilik Politikamızı dikkatlice okuyun.
+</p>',
+        ],
+        [
+            'title' => '3. Uygulama Kullanım Lisansı',
+            'content' => '<p>
+    Size, bu Şartlara tabi olmak kaydıyla, kişisel, ticari olmayan kullanımınız için Uygulamayı indirme, kurma ve kullanma konusunda iptal edilebilir, münhasır olmayan, devredilemez, sınırlı bir lisans veriyoruz.
+</p>',
+        ],
+        [
+            'title' => '4. Kullanıcı Hesapları',
+            'content' => '<p>
+    Uygulamamızın herhangi bir bölümü sizden bir hesap oluşturmanızı gerektiriyorsa, doğru ve eksiksiz bilgi sağlamalısınız.
+    Hesap bilgilerinizin, şifreniz de dahil olmak üzere, gizliliğini korumaktan ve hesabınız altında gerçekleşen tüm etkinliklerden siz sorumlusunuz.
+</p>',
+        ],
+        [
+            'title' => '5. Kullanıcı Davranışı',
+            'content' => '<p>
+    Uygulamamızı yalnızca yasal amaçlarla ve başkalarının Uygulamayı kullanımını ve keyfini sınırlamayan veya engellemeyen bir şekilde kullanmayı kabul ediyorsunuz.
+    Yasaklanan davranışlar arasında başka bir kullanıcıyı taciz etmek veya rahatsızlık vermek, müstehcen veya saldırgan içerik göndermek veya Uygulama içindeki normal diyaloğun akışını bozmak yer alır.
+</p>',
+        ],
+        [
+            'title' => '6. Fikri Mülkiyet Hakları',
+            'content' => '<p>
+    Uygulama ve orijinal içeriği, özellikleri ve işlevselliği Nuntechs ve lisans verenlerinin münhasır mülkiyeti olarak kalacaktır.
+    Fikri mülkiyetimiz, tarafımızdan yetkilendirilmemiş herhangi bir şekilde kullanılamaz.
+</p>',
+        ],
+        [
+            'title' => '7. Fesih',
+            'content' => '<p>
+    Hesabınızı herhangi bir nedenle, sınırlama olmaksızın, Şartların ihlali dahil ancak bunlarla sınırlı olmaksızın, tamamen kendi takdirimize bağlı olarak, derhal feshedebilir veya askıya alabilir ve Uygulamaya erişimi engelleyebiliriz.
+</p>',
+        ],
+        [
+            'title' => '8. Sorumluluğun Sınırlandırılması',
+            'content' => '<p>
+    Nuntechs, yöneticileri, çalışanları, ortakları, ajanları, tedarikçileri veya iştirakleri, Uygulamanın veya hizmetlerin kullanımından kaynaklanan herhangi bir dolaylı, tesadüfi, özel, sonuç olarak ortaya çıkan veya cezai zararlar, kar kaybı, veri kaybı, kullanım kaybı, itibar kaybı veya diğer maddi olmayan kayıplar için sorumlu olmayacaktır.
+</p>',
+        ],
+        [
+            'title' => '9. Şartlarda Değişiklikler',
+            'content' => '<p>
+    Bu Şartları herhangi bir zamanda, tamamen kendi takdirimize bağlı olarak, değiştirme veya yenileme hakkını saklı tutarız.
+    Bu revizyonlar yürürlüğe girdikten sonra Uygulamamıza erişmeye veya kullanmaya devam ederek, revize edilmiş şartlarla bağlı kalmayı kabul edersiniz.
+</p>',
+        ],
+        [
+            'title' => '10. Geçerli Hukuk',
+            'content' => '<p>
+    Bu Şartlar, kanunlar ihtilafı hükümleri dikkate alınmaksızın, Polonya yasalarına göre yönetilecek ve yorumlanacaktır.
+</p>',
+        ],
+        [
+            'title' => '11. Bizimle İletişime Geçin',
+            'content' => '<p>
+        Bu Şartlar hakkında herhangi bir sorunuz varsa, lütfen e-posta yoluyla bizimle iletişime geçmekten çekinmeyin <a href="mailto:admin@nuntech.com">admin@nuntech.com</a>.
+    </p>',
+        ],
+    ]
+];
